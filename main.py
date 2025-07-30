c8c140a5c5a6dc3a9b9a34fa2c88fbd2402d185f
casazza07 <henriquecasazza00@gmail.com>
2025-07-30 14:35:44 -0300

    main.py

--- main.py
+++ main.py
@@ -28,4 +28,6 @@ def home():
     return "Servidor do bot rodando!"
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
